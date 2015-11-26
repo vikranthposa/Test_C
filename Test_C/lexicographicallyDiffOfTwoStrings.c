b9a154cc76a59370c7e123b6f1c7fd882a747be4
vikranthposa <vikranth.iitk@gmail.com>
2015-11-26 19:08:11 +0530

    lexicographicallyDiffOfTwoStrings
     Write a function compare() that works similar to strcmp(), i.e., it
    returns 0 if both strings are same, 1 if first string is
    lexicographically greater, and -1 if second string is lexicographically
    greater.

---
+++ Test_C/lexicographicallyDiffOfTwoStrings.c
@@ -0,0 +1,53 @@
+//
+//  lexicographicallyDiffOfTwoStrings.c
+//  Test_C
+//
+//  Created by Vikranth Posa on 25/11/15.
+//  Copyright (c) 2015 Vikranth Posa. All rights reserved.
+//
+
+#include <stdio.h>
+#include <string.h>
+
+int getLowerAsciValue(char ch)
+{
+    int diff = 'A'-'a';
+    if (ch >='a' &&  ch<='z') {
+        return ch;
+    }
+    else if (ch>='A' && ch<='Z') {
+        return ch-diff;
+    }
+    return ch;
+}
+
+int getLexiDiff(char str1[], char str2[])
+{
+    int l1 = (int) strlen(str1);
+    int l2 = (int) strlen(str2);
+    int i;
+    for (i=0; (i<l1 && i<l2); i++) {
+        if (getLowerAsciValue(str1[i]) - getLowerAsciValue(str2[i])) {
+            return ((str1[i] - str2[i])>0?1:-1);
+        }
+    }
+    
+    if (l1==l2) {
+        return 0;
+    } else {
+        return ((l1-l2)>0?1:-1);
+    }
+    
+}
+
+
+int main()
+{
+    char str1[] = "vikr";
+    char str2[] = "VIK!R";
+    int lexiDiff = getLexiDiff(str1,str2);
+    printf("LexiCographical Difference = %d" , lexiDiff);
+    return 0;
+    
+}
+
--- Test_C/main.c
+++ Test_C/main.c
@@ -8,25 +8,6 @@
 
 #include <stdio.h>
 #include <string.h>
-#include "checkEditDistance.h"
 
-
-
-int main()
-{
-    
-    char *s1 = "geek";
-    char *s2 = "geeks";
-    
-    unsigned long int n = checkEditDistance(s1,s2);
-    if (n<=1) {
-        printf("distance  is not more than 1 ");
-    }
-    else
-    {
-        printf("distance = %ld , ie., is more than 1",n);
-    }
-    return 0;
-}
 
 
